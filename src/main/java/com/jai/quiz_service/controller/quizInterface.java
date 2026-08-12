0a0d954a8977fd2603b0d1331a4c4958267ac03a
jayakrishna9655 <jayakrishna96555@gmail.com>
2026-08-12 23:16:02 +0530

    first commit

---
+++ src/main/java/com/jai/quiz_service/controller/quizInterface.java
@@ -0,0 +1,28 @@
+package com.jai.quiz_service.controller;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.jai.quiz_service.model.QuestionWrapper;
+import com.jai.quiz_service.model.Response;
+
+
+@FeignClient("QUESTION-SERVICE")
+public interface quizInterface {
+
+	@GetMapping("/generate")
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz(@RequestParam String category,@RequestParam Integer numOfQuestions);
+    
+    @PostMapping("/getQuetion")
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionId);
+    
+    @PostMapping("/getScore")
+    public ResponseEntity<Integer> getScore(@RequestBody List<Response> responses);
+	
+}
